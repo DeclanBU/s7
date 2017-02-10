c6a7260da6eefd0eced3722c8c895b45d316c47f
Declan G. Buckley <t00189550@365s.ittralee.ie>
2017-02-10 12:47:15 +0000

    Initial commmit
    Added .gitignore
    Added workbook1.pynb
    Added wheream.sh

---
+++ whereami.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#i use command expansion
+echo "you are in /home/t00189550"
+
+echo "total 29
+-rwxr-xr-x  1 Administrators Domain Users 12470 Feb 10 11:33 01_bash_setting_permissions.ipynb
+drwxr-xr-x+ 1 t00189550      Domain Users     0 Feb 10 11:42 s7
+-rwxr--r--  1 t00189550      Domain Users    47 Feb 10 12:03 whereami.sh
+-rwxr-xr-x  1 t00189550      Domain Users  8943 Feb 10 12:04 workbook1.ipynb"
+
+
